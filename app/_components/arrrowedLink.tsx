688d4b81fac2783d1c1339b31ebceccf6829da08
Carmen Ng <carmenng.media@gmail.com>
2024-01-28 10:35:22 +0800

    Add 404 page

--- app/_components/arrrowedLink.tsx
+++ app/_components/arrrowedLink.tsx
@@ -1,9 +1,13 @@
 import { EastRounded } from "@mui/icons-material";
 import Link from "next/link";
 
-export default function ArrowedLink({ title, link }) {
+export default function ArrowedLink({ title, link, stay }) {
   return (
-    <Link className="flex gap-2 items-center" href={link} target="_blank">
+    <Link
+      className="flex gap-2 items-center"
+      href={link}
+      target={stay ? "_self" : "_blank"}
+    >
       {title}
       <EastRounded sx={{ fontSize: "18px" }} />
     </Link>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,22 @@
-import Link from "next/link";
+import ArrowedLink from "./_components/arrrowedLink";
+import ParticlesBackground from "./_components/particlesBackground";
+import TerminalText from "./_components/terminalText";
 
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <ParticlesBackground />
+      <div className="flex min-w-screen min-h-screen justify-center items-center px-5">
+        <div className="flex flex-col w-full items-center">
+          <div className="z-10 text-9xl font-extrabold mb-5 italic mt-20">
+            404
+          </div>
+          <TerminalText />
+          <div className="z-10 mt-14">
+            <ArrowedLink link="/" title="return home" stay />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
