d87dca90c21badecdcb7a69ac693be034382b2ad
Carmen Ng <carmenng.media@gmail.com>
2024-01-28 10:56:32 +0800

    Fix typescript error

--- app/_components/arrrowedLink.tsx
+++ app/_components/arrrowedLink.tsx
@@ -1,14 +1,18 @@
 import { EastRounded } from "@mui/icons-material";
 import Link from "next/link";
 
-export default function ArrowedLink({ title, link, stay }) {
+export default function ArrowedLink(props: {
+  title: string;
+  link: string;
+  stay?: boolean;
+}) {
   return (
     <Link
       className="flex gap-2 items-center"
-      href={link}
-      target={stay ? "_self" : "_blank"}
+      href={props.link}
+      target={props.stay ? "_self" : "_blank"}
     >
-      {title}
+      {props.title}
       <EastRounded sx={{ fontSize: "18px" }} />
     </Link>
   );
