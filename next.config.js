d5cd379511cdfdbdf25ca656e5526fb04740160e
Carmen Ng <carmenng.media@gmail.com>
2024-01-30 18:09:48 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/abst",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
