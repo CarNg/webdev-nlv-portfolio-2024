343a45e82e16141b20723848df009485efaaf328
Carmen Ng <carmenng.media@gmail.com>
2024-01-30 19:34:46 +0800

    Update active links on navbar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -5,19 +5,21 @@ const links = ["about", "projects", "contact"];
 
 export default function Navbar({ hide }) {
   return (
-    <div className="fade-in-anin flex w-screen max-w-full py-6 px-8 md:px-12 justify-between">
+    <div className="fade-in-anin flex w-screen max-w-full py-6 px-7 md:px-12 justify-between">
       <Link className="z-20" href="/">
         <ArrowBackIosNewRounded sx={{ fontSize: { xs: "18px", md: "24px" } }} />
       </Link>
       <div className="z-20 flex gap-5">
         {links.map((link) => {
-          if (link !== hide) {
-            return (
-              <Link key={link} href={`/${link}`}>
-                {link}
-              </Link>
-            );
-          }
+          return (
+            <Link
+              key={link}
+              href={`/${link}`}
+              className={hide === link ? "text-dark-teal italic" : ""}
+            >
+              {link}
+            </Link>
+          );
         })}
       </div>
     </div>
