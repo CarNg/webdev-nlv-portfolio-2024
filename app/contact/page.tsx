ad2e9d368eb62a74143f8bc8eaa08834efc7804e
Carmen Ng <carmenng.media@gmail.com>
2024-01-30 19:35:44 +0800

    Minor updates to Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,8 +5,8 @@ import {
 } from "@/lib/api/fetchContactPage";
 import { Metadata } from "next";
 import Link from "next/link";
-import Navbar from "../_components/navbar";
-import ParticlesBackground from "../_components/particlesBackground";
+import Navbar from "../_components/Navbar";
+import ParticlesBackground from "../_components/ParticlesBackground";
 import IconEmail from "../assets/IconEmail";
 import IconGithub from "../assets/IconGithub";
 import IconItchIo from "../assets/IconItchIo";
@@ -49,7 +49,7 @@ const getIcon = (type: string) => {
   }
 };
 
-export default async function About() {
+export default async function Contact() {
   const data = await getContactPageData();
 
   return (
@@ -66,7 +66,7 @@ export default async function About() {
                 target="_blank"
                 className="w-full md:w-auto"
               >
-                <div className="z-10 flex flex-col gap-4 border-white border-solid border-2 rounded-lg p-6 md:p-10 md:min-w-72 md:w-72 md:min-h-72 h-40 md:h-72 justify-center md:items-center break-words link-box">
+                <div className="z-10 flex flex-col gap-4 border-white border-solid border-2 rounded-lg p-6 md:p-10 md:min-w-72 md:w-72 md:min-h-72 h-36 md:h-72 justify-center md:items-center break-words link-box bg-black">
                   <div className="flex flex-row md:flex-col items-center gap-4">
                     {getIcon(c.type)}
                     <span className="text-sm exception md:hidden">
